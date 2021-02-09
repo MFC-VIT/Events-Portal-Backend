505e8a68eb5abdd13119673ec3b8b2c200d74124
Akshat Gupta <akshatgupta1903@gmail.com>
2021-02-09 20:29:32 +0530

    Update utils.py

--- events/eventsapi/utils.py
+++ events/eventsapi/utils.py
@@ -7,5 +7,5 @@ import yagmail
 class Util:
     @staticmethod
     def send_email(data):
-        yag = yagmail.SMTP(user="shubhakshat10@gmail.com", password="Gupta2002")
+        yag = yagmail.SMTP(user=os.environ.get('EMAIL_HOST_USER'), password=os.environ.get('EMAIL_HOST_PASSWORD'))
         yag.send(to=data['to_email'], subject=data['email_subject'], contents=data['email_body'])
