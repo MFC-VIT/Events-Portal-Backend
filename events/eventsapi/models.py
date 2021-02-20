2e8a8253660edc26809116357828107275ff1299
Akshat1903 <akshatgupta1903@gmail.com>
2021-02-20 19:25:55 +0530

    Event additional models added

--- events/eventsapi/models.py
+++ events/eventsapi/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 from django.core.validators import MaxValueValidator, MinValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
+from ckeditor.fields import RichTextField
 
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -43,12 +44,32 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
+class UserDetails(models.Model):
+    pass
+
 class Event(models.Model):
     name = models.CharField(max_length=255,unique=True)
     registered_users = models.ManyToManyField(UserAccount,through="Event_Registration")
 
     def __str__(self):
         return self.name
+
+class EventDetails(models.Model):
+    event = models.OneToOneField(Event, on_delete=models.CASCADE, related_name='event_description')
+    event_date = models.DateField()
+    description = RichTextField(blank=True,null=True)
+    main_image = models.TextField()
+    banner_image = models.TextField()
+
+    def __str__(self):
+        return self.event.name
+
+class EventGallery(models.Model):
+    event = models.ForeignKey(Event,on_delete=models.CASCADE,related_name="event_gallery")
+    gallery_image = models.TextField()
+
+    def __str__(self):
+        return self.event.name
 
 class Event_Registration(models.Model):
     event = models.ForeignKey(Event,related_name="event_registration",on_delete=models.CASCADE)
